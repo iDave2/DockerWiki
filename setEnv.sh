7348ad47dafb5987b48e5e734dcd1dc41120a4fe
iDave2 <dave.king@pobox.com>
2023-08-20 17:35:55 -0600

    initial commit

---
+++ setEnv.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+#
+#  Source this file for convenient shortcuts.
+#
+####-####+####-####+####-####+####-####+####-####+####-####+####-####+####
+
+# https://stackoverflow.com/a/246128
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+alias br="${SCRIPT_DIR}/bin/backrest.sh"
+alias cake="${SCRIPT_DIR}/bin/cake.sh"
+alias d=docker
+alias dc="docker compose"
