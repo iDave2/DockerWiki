1cae202b88f67691a016ac1082d74760f5d3616c
iDave2 <dave.king@pobox.com>
2023-08-25 16:15:39 -0600

    checkpoint

--- bin/backrest.sh
+++ bin/backrest.sh
@@ -3,7 +3,7 @@
 #  Ideas for data backup and restore.
 #  Also see https://hub.docker.com/_/mariadb.
 #  This script passes cleartext passwords so is Not Secure.
-#  This script requires bash and the ever useful jq.
+#  This script requires 'bash' and the always delightful 'jq'.
 #
 ####-####+####-####+####-####+####-####+####-####+####-####+####-####+####
 
@@ -13,23 +13,21 @@ SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 source "${SCRIPT_DIR}/include.sh"
 source "${SCRIPT_DIR}/../.env"
 
-# What are we even doing here?
+# What to do.
 BACKUP=false
+QUIET=false
 RESTORE=false
 
-# Where to backup to or restore from.
-WORK_DIR=${BACKUP_DIR}
+# Where to do it.
+WORK_DIR=${TEMP_DIR}
 DATA_FILE="${WORK_DIR}/all-databases.sql"
 IMAGE_DIR="${WORK_DIR}/images"
 
 # Constants used to validate incoming data and view container names.
 DATA_HOST_NAME() { echo data; }
-DATA_IMAGE_NAME() { echo mariadb; } # Actually $DID/mariadb, see below.
+DATA_IMAGE_NAME() { echo mariadb; }
 VIEW_HOST_NAME() { echo view; }
 VIEW_IMAGE_NAME() { echo mediawiki; }
-
-# [ mariadb == $(DATA_IMAGE_NAME) ] && echo Match || echo No Match
-# exit 2
 
 ####-####+####-####+####-####+####-####+
 #
@@ -43,14 +41,14 @@ checkContainer() {
   local commandB="jq --raw-output '.[0].Config.Hostname, .[0].Config.Image'"
   xShow $commandA '|' $commandB
 
-  local errFile='/tmp/stderr'
+  local errFile="${TEMP_DIR}/stderr"
   local inspect=$($commandA 2>$errFile) # docker $? always zero...
   local error=$(<$errFile)
   [ -n "$error" ] && usage $error
 
   local hostTest=$(echo "$inspect" | jq --raw-output '.[0].Config.Hostname')
   local imageTest=$(echo "$inspect" | jq --raw-output '.[0].Config.Image')
-  # remove $DID at front and :tag at end
+  # idave2/mariadb:0.2.0 => mariadb
   imageTest=$(echo $(basename $imageTest) | sed -e s/:.*//)
 
   if [ "$hostTest" != "$hostName" ]; then
@@ -70,56 +68,40 @@ main() {
   parseCommandLine "$@"
 
   ! $BACKUP && ! $RESTORE &&
-    usage Please specify --backup or --restore \(or both FWIW\)
+    usage "Please specify --backup or --restore (or both FWIW)"
 
-  # Are containers fairly valid?
+  xCute mkdir $WORK_DIR $IMAGE_DIR # 2>/dev/null
+
   checkContainer ${DATA_CONTAINER} $(DATA_HOST_NAME) $(DATA_IMAGE_NAME)
   checkContainer ${VIEW_CONTAINER} $(VIEW_HOST_NAME) $(VIEW_IMAGE_NAME)
-  return 99
-  local command="docker container inspect '$DATA_CONTAINER' | jq '.[0].Config.Env' | grep MARIADB_DATABASE"
-  xShow $command
-  local dbName=$(eval $command)
-  echo $dbName
-  if [[ "$dbName" == *mediawiki* ]]; then
-    echo && echo "# Using data container '$DATA_CONTAINER'"
-  else
-    usage "Expected to find 'mediawiki' database in data container '$DATA_CONTAINER'; found '$dbName' instead"
-    return -17
+
+  # Add a pause here? "Press enter to continue with this config?"
+  if true; then
+    echo
+    echo "DATA_CONTAINER = '$DATA_CONTAINER'"
+    echo "VIEW_CONTAINER = '$VIEW_CONTAINER'"
+    echo "WORK_DIR = '$WORK_DIR'"
+    echo "DATA_FILE = '$DATA_FILE'"
+    echo "IMAGE_DIR = '$IMAGE_DIR'"
   fi
 
-  return 42
-
-  # Is view container compelling? https://stackoverflow.com/a/12973694
-  command="docker container inspect '$VIEW_CONTAINER' | jq '.[0].Config.Env' | grep MEDIAWIKI_VERSION | xargs"
-  xShow $command
-  local version=$(eval $command)
-  echo $version
-  if [ -n "$version" ]; then
-    echo && echo "# Using data container '$DATA_CONTAINER'"
-  else
-    usage "Expected to find MEDIAWIKI_VERSION in view container '$VIEW_CONTAINER'; found '$version' instead"
-    return -18
-  fi
-
-  # [ -d "$WORK_DIR" ] || mkdir "$BACKUP_DIR"
-  # [ -d "$IMAGE_DIR"] || mkdir "$IMAGE_DIR"
-
-  echo "DATA_CONTAINER = '$DATA_CONTAINER'"
-  echo "VIEW_CONTAINER = '$VIEW_CONTAINER'"
-  echo "WORK_DIR = '$WORK_DIR'"
-  echo "DATA_FILE = '$DATA_FILE'"
-  echo "IMAGE_DIR = '$IMAGE_DIR'"
-
-  return 42
-
-  # Backup and restore.
   if $BACKUP; then
-    xOut "$DATA_FILE" docker exec "$CONTAINER" sh -c "mariadb-dump --all-databases -uroot -p$MARIADB_ROOT_PASSWORD"
-    local command="docker run --rm ${VIEW_CONTAINER} tar -cC /var/www/html/images . | tar -xC ${BACKUP_DIR}/images"
-    xShow $command
+
+    xOut "$DATA_FILE" docker exec "$DATA_CONTAINER" sh -c "mariadb-dump --all-databases -uroot -p$MARIADB_ROOT_PASSWORD"
+
+    local commandA="docker exec $VIEW_CONTAINER tar -cC /var/www/html/images ."
+    local commandB="tar -xC ${WORK_DIR}/images"
+    xShow "$commandA | $commandB"
+    $commandA | $commandB
+
+    echo && echo "Wiki backup written to '$WORK_DIR'"
+
   fi
+
   if $RESTORE; then
-    xIn "$DATA_FILE" docker exec -i "$CONTAINER" sh -c "exec mariadb -uroot -p$MARIADB_ROOT_PASSWORD"
+
+    xIn "$DATA_FILE" docker exec -i "$DATA_CONTAINER" sh -c "exec mariadb -uroot -p$MARIADB_ROOT_PASSWORD"
+
   fi
 }
 
@@ -172,7 +154,7 @@ parseCommandLine() {
 #
 usage() {
   if [ -n "$*" ]; then
-    echo && echo "==> $*"
+    echo && echo "****  $*  ****"
   fi
   cat <<-EOT
 
@@ -186,9 +168,13 @@ Options:
   -h | --help                     Print this usage summary
   -r | --restore                  Restore database and images
   -v | --view-container  string   View container, overrides .env
-  -w | --work-dir  string         Work area, overrides .env/BACKUP_DIR
+  -w | --work-dir  string         Work area, overrides .env/TEMP_DIR
 EOT
   exit 42
 }
 
+####-####+####-####+####-####+####-####+
+#
+#  Capua, shall I begin?
+#
 main "$@"
