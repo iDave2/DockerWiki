f8fb71661961a5d3f902ad3618a223836545ff3e
iDave2 <dave.king@pobox.com>
2023-08-25 06:01:11 -0600

    backrest, include images please

--- bin/backrest.sh
+++ bin/backrest.sh
@@ -13,9 +13,11 @@ source "${SCRIPT_DIR}/include.sh"
 source "${SCRIPT_DIR}/../.env"
 
 BACKUP=false
-CONTAINER=
-FILE=$BACKUP_FILE
 RESTORE=false
+
+WORK_DIR=${BACKUP_DIR}
+DATA_FILE="${WORK_DIR}/all-databases.sql"
+IMAGE_DIR="${WORK_DIR}/images"
 
 main() {
 
@@ -23,15 +25,14 @@ main() {
 
   # Parse command line, https://stackoverflow.com/a/14203146.
 
-  CONTAINERS=()
   while [[ $# -gt 0 ]]; do
     case "$1" in
     -b | --backup)
       BACKUP=true
       shift
       ;;
-    -f | --file)
-      FILE="$2"
+    -d | --data-container)
+      DATA_CONTAINER="$2"
       shift 2
       ;;
     -h | --help)
@@ -42,18 +43,37 @@ main() {
       RESTORE=true
       shift
       ;;
+    -v | --view-container)
+      VIEW_CONTAINER="$2"
+      shift 2
+      ;;
+    -w | --work-dir)
+      WORK_DIR="$2"
+      shift 2
+      DATA_FILE="${WORK_DIR}/all-databases.sql"
+      IMAGE_DIR="${WORK_DIR}/images"
+      ;;
     -* | --*)
       usage unknown option \"$1\"
       return 41
       ;;
     *)
-      CONTAINERS+=("$1")
-      shift
+      usage "unexpected argument '$1'"
+      return 42
       ;;
     esac
   done
 
-  # echo backup[$BACKUP], restore[$RESTORE], file[$FILE], containers[\"$(join '" "' "${CONTAINERS[@]}")\"]
+  # [ -d "$WORK_DIR" ] || mkdir "$BACKUP_DIR"
+  # [ -d "$IMAGE_DIR"] || mkdir "$IMAGE_DIR"
+
+  echo "DATA_CONTAINER = '$DATA_CONTAINER'"
+  echo "VIEW_CONTAINER = '$VIEW_CONTAINER'"
+  echo "WORK_DIR = '$WORK_DIR'"
+  echo "DATA_FILE = '$DATA_FILE'"
+  echo "IMAGE_DIR = '$IMAGE_DIR'"
+
+  # echo backup[$BACKUP], restore[$RESTORE], file[$DATA_FILE], containers[\"$(join '" "' "${CONTAINERS[@]}")\"]
 
   if ! $BACKUP && ! $RESTORE; then
     usage Specify --backup or --restore \(or both FWIW\)
@@ -74,14 +94,16 @@ main() {
     ;;
   esac
 
-  # echo backup[$BACKUP], restore[$RESTORE], file[$FILE], container[$CONTAINER]
+  # echo backup[$BACKUP], restore[$RESTORE], file[$DATA_FILE], container[$CONTAINER]
 
   # Backup and restore.
   if $BACKUP; then
-    xOut "$FILE" docker exec "$CONTAINER" sh -c "mariadb-dump --all-databases -uroot -p$MARIADB_ROOT_PASSWORD"
+    xOut "$DATA_FILE" docker exec "$CONTAINER" sh -c "mariadb-dump --all-databases -uroot -p$MARIADB_ROOT_PASSWORD"
+    local command="docker run --rm ${VIEW_CONTAINER} tar -cC /var/www/html/images . | tar -xC ${BACKUP_DIR}/images"
+    xShow $command
   fi
   if $RESTORE; then
-    xIn "$FILE" docker exec -i "$CONTAINER" sh -c "exec mariadb -uroot -p$MARIADB_ROOT_PASSWORD"
+    xIn "$DATA_FILE" docker exec -i "$CONTAINER" sh -c "exec mariadb -uroot -p$MARIADB_ROOT_PASSWORD"
   fi
 }
 
@@ -96,10 +118,12 @@ Usage: $(basename ${BASH_SOURCE[0]}) [OPTIONS] DATABASE_CONTAINER
 Backup and restore a MariaDB database.
 
 Options:
-  -b | --backup         Backup a database
-  -f | --file  string   File to backup to or restore from
-  -h | --help           Print this usage summary
-  -r | --restore        Restore a database
+  -b | --backup                   Backup database and images
+  -d | --data-container  string   Data container, overrides .env
+  -h | --help                     Print this usage summary
+  -r | --restore                  Restore database and images
+  -v | --view-container  string   View container, overrides .env
+  -w | --work-dir  string         Work area, overrides .env/BACKUP_DIR
 EOT
   return 42
 }
