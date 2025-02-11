c51f104a6303a0428b53c0426cfc32acb322cb88
iDave2 <idave2.king@gmail.com>
2025-02-11 06:28:49 -0700

    refac

---
+++ mariadb/20-noop.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+#
+# This MariaDB initialization script does nothing but verify that it runs
+# when dropped into container's '/docker-entrypoint-initdb.d/' folder as
+# advertised on https://hub.docker.com/_/mariadb.
+
+_join() { # https://stackoverflow.com/a/17841619
+  c="" d="$1" r=""; shift
+  for t in "$@"; do r="$r$c$t"; c=$d; done # or 'echo -n "$c$t"'
+  echo $r
+}
+
+echo my-entry-point: [ $(_join ', ' "$0" "$@") ]
+
+# bash
